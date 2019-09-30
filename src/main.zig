54b52d720cb37215b08f8fd13010681c035b10b0
deing <admin@15318.de>
2019-09-30 15:42:03 +0200

    Add guessing game

--- src/main.zig
+++ src/main.zig
@@ -12,6 +12,9 @@ pub fn main() void {
     std.debug.warn("Fourier Sequence:\n");
     fourier_sequence(&vm);
     vm.dump();
+    std.debug.warn("Guessing Game:\n");
+    guessing_game(&vm);
+    vm.dump();
 }
 
 fn global_instruction(comptime op: GlobalOpcode, comptime addr: u12) u16 {
@@ -93,4 +96,62 @@ fn fourier_sequence(vm: *OidaVm) void {
     vm.load(0x15, extended_instruction(.OutputLinefeed));
     vm.load(0x16, paged_instruction(.Jump, 0x6));
     vm.eval(0x7);
+}
+
+fn guessing_game(vm: *OidaVm) void {
+    vm.flush();
+
+    const _val = 0x001;
+    const _val_minus_one = 0x002;
+    const _backup = 0x005;
+
+    const init = 0x00a;
+    const start = 0x00f;
+    const maybe_yay = 0x040;
+    const too_low_point = 0x060;
+
+    const too_big = 0x100;
+    const too_low = 0x200;
+    const yay = 0x300;
+
+    var rand = std.rand.DefaultPrng.init(std.time.timestamp()).random;
+    var randint = rand.int(u8);
+
+    vm.load(_val, randint);
+    vm.load(_val_minus_one, randint - 1);
+    vm.load(init, global_instruction(.PageAndJump, init + 1));
+    vm.load(init + 1, paged_instruction(.Jump, start));
+
+    vm.load(start, extended_instruction(.InputACC));
+    vm.load(start + 1, paged_instruction(.Write, _backup));
+    vm.load(start + 2, paged_instruction(.Minus, _val));
+    vm.load(start + 3, paged_instruction(.JumpEZ, maybe_yay));
+    vm.load(start + 4, global_instruction(.PageAndJump, too_big));
+
+    vm.load(maybe_yay, paged_instruction(.Fetch, _backup));
+    vm.load(maybe_yay + 1, paged_instruction(.Minus, _val_minus_one));
+    vm.load(maybe_yay + 2, paged_instruction(.JumpEZ, too_low_point));
+    vm.load(maybe_yay + 3, global_instruction(.PageAndJump, yay));
+
+    vm.load(too_low_point, global_instruction(.PageAndJump, too_low));
+
+    vm.load(too_big, paged_instruction(.Fetch, 10));
+    vm.load(too_big + 1, extended_instruction(.OutputChar));
+    vm.load(too_big + 2, extended_instruction(.OutputLinefeed));
+    vm.load(too_big + 3, global_instruction(.PageAndJump, start));
+    vm.load(too_big + 10, '+');
+
+    vm.load(too_low, paged_instruction(.Fetch, 10));
+    vm.load(too_low + 1, extended_instruction(.OutputChar));
+    vm.load(too_low + 2, extended_instruction(.OutputLinefeed));
+    vm.load(too_low + 3, global_instruction(.PageAndJump, start));
+    vm.load(too_low + 10, '-');
+
+    vm.load(yay, paged_instruction(.Fetch, 10));
+    vm.load(yay + 1, extended_instruction(.OutputChar));
+    vm.load(yay + 2, extended_instruction(.OutputLinefeed));
+    vm.load(yay + 3, extended_instruction(.Halt));
+    vm.load(yay + 10, '=');
+
+    vm.eval(init);
 }
